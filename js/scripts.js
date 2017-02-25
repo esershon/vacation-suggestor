63c7ba0ce4fdb0e6e13e82df64a0bb7ac57790bd
Emily Sershon <emily.sershon@gmail.com>
2017-02-24 21:54:29 -0800

    if/else logic working

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,27 @@ $(document).ready(function() {
 
    var breathing = $("select#breathing").val();
    var pressure = $("select#pressure").val();
-   var margin = $("input[type=radio][name=marginalized]:checked").val();
+   var marginalized = $("input[type=radio][name=marginalized]:checked").val();
    var children = $("input[type=radio][name=children]:checked").val();
    console.log("breathing method: " + breathing);  // for debugging
    console.log("pressure tolerance: " + pressure);  // for debugging
-   console.log("marginalized class: " + margin);  // for debugging
-   console.log("travelling with children: " + children);  // for debugging
+   console.log("marginalized class: " + marginalized);  // for debugging
+   console.log("traveling with children: " + children);  // for debugging
 
-if (breathing === "Gills"){
-  $("#thesea").show();
-} else if (margin === "no" && children === "yes") {
-  $("#timetravel").show();
-} else{
-  $('#plzstayhome').show();
+   if (breathing === "gills"){ // If you don't have lungs, go to the sea
+     $("#thesea").show();
+   } else if (marginalized === "no" && children === "yes") { // If you have lungs, privilege, and children, time travel.
+     $("#timetravel").show();
+   } else if (marginalized === "no" && children === "no" && pressure != "low") { //If you have lungs, privilege, no children, but you're not good with low pressures, time travel.
+     $('#timetravel').show();
+   } else if (marginalized === "no" && children === "no") { //If you have lungs, privilege, no children, and you are ok with low pressures, go to Mars.
+     $('#mars').show();
+   } else if (breathing === "both" && (pressure != "low" && children === "no")) { 
+     $('#thesea').show();
+   } else if (children === "no") {
+     $('#mars').show();
+   } else {
+     $('#plzstayhome').show();
 }
 
   event.preventDefault();
