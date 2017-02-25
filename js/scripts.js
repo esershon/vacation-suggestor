1935432e799ad0d31bf920be296e9b1639901c38
Emily Sershon <emily.sershon@gmail.com>
2017-02-24 23:57:30 -0800

    add copy and images for each branch, and hide form on submission, and add button to try again

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+ $("button#again").click(function(){
+   $('#questions').show();
+   $('#results').hide();
+ });
   $("form#vacation").submit(function(event) {
     var name = $("input#name").val();
     var breathing = $("select#breathing").val();
@@ -14,24 +18,34 @@ $(document).ready(function() {
     if (name){
    $("#results").show();
    $('#thesea, #timetravel, #mars, #plzstayhome').hide();
+   $('#questions').hide();
    $(".name").text(name);
 
    if (breathing === "gills"){ // If you don't have lungs, go to the sea
      $("#thesea").show();
+     $('.vacationSpot').text("The Ocean Depths");
    } else if (marginalized === "no" && children === "yes") { // If you have lungs, privilege, and children, time travel.
      $("#timetravel").show();
+     $('.vacationSpot').text("Time Travel to the Past");
    } else if (marginalized === "no" && children === "no" && pressure != "low") { //If you have lungs, privilege, no children, but you're not good with low pressures, time travel.
      $('#timetravel').show();
+     $('.vacationSpot').text("Time Travel to the Past");
    } else if (marginalized === "no" && children === "no") { //If you have lungs, privilege, no children, and you are ok with low pressures, go to Mars.
      $('#mars').show();
+     $('.vacationSpot').text("Mars");
    } else if (breathing === "both" && pressure != "low" && children === "no") {
      $('#thesea').show();
+     $('.vacationSpot').text("The Ocean Depths");
    } else if (children === "no") {
      $('#mars').show();
+     $('.vacationSpot').text("Mars");
    } else if (children === "yes" && breathing === "both"){
      $('#thesea').show();
+     $('.vacationSpot').text("The Ocean Depths");
    } else {
      $('#plzstayhome').show();
+     $('.vacationSpot').text("Nah, Nevermind");
+
 }
 } else [
   alert("Don't forget to enter your name!")
