23872e4a73398ec561a814c01d3d45455de1f0f8
Emily Sershon <emily.sershon@gmail.com>
2017-02-24 22:44:42 -0800

    de-bug all javascript logic, allow successive submissions, prompt user to enter name if field left blank

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
   $("form#vacation").submit(function(event) {
-   $("#results").show();
+    var name = $("input#name").val();
+    var breathing = $("select#breathing").val();
+    var pressure = $("select#pressure").val();
+    var marginalized = $("input[type=radio][name=marginalized]:checked").val();
+    var children = $("input[type=radio][name=children]:checked").val();
 
-   var breathing = $("select#breathing").val();
-   var pressure = $("select#pressure").val();
-   var marginalized = $("input[type=radio][name=marginalized]:checked").val();
-   var children = $("input[type=radio][name=children]:checked").val();
-   console.log("breathing method: " + breathing);  // for debugging
-   console.log("pressure tolerance: " + pressure);  // for debugging
-   console.log("marginalized class: " + marginalized);  // for debugging
-   console.log("traveling with children: " + children);  // for debugging
+    console.log("breathing method: " + breathing);  // for debugging
+    console.log("pressure tolerance: " + pressure);  // for debugging
+    console.log("marginalized class: " + marginalized);  // for debugging
+    console.log("traveling with children: " + children);  // for debugging
+
+    if (name){
+   $("#results").show();
+   $('#thesea, #timetravel, #mars, #plzstayhome').hide();
+   $(".name").text(name);
 
    if (breathing === "gills"){ // If you don't have lungs, go to the sea
      $("#thesea").show();
@@ -19,25 +24,18 @@ $(document).ready(function() {
      $('#timetravel').show();
    } else if (marginalized === "no" && children === "no") { //If you have lungs, privilege, no children, and you are ok with low pressures, go to Mars.
      $('#mars').show();
-   } else if (breathing === "both" && (pressure != "low" && children === "no")) { 
+   } else if (breathing === "both" && pressure != "low" && children === "no") {
      $('#thesea').show();
    } else if (children === "no") {
      $('#mars').show();
+   } else if (children === "yes" && breathing === "both"){
+     $('#thesea').show();
    } else {
      $('#plzstayhome').show();
 }
-
+} else [
+  alert("Don't forget to enter your name!")
+]
   event.preventDefault();
   });
 });
-
-//if (animal === "snakes"){
-//  $('#snakes').show();
-//} else if (animal === "turtles"){
-//    $('#turtles').show();
-//  } else if (animal === "insects"){
-//    $('#insects').show();
-//  }else {
-//    alert("TRY AGAIN");
-//    window.location.reload();
-//  }
